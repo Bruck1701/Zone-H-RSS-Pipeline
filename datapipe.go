b6add59954daeb18e1a2b394951615ad6787e0ef
bruck <kummel@gmail.com>
2019-05-27 18:04:50 -0300

    Added the storage stage of the pipeline. First version will store the the gathered data on ElasticSearch

--- datapipe.go
+++ datapipe.go
@@ -7,6 +7,14 @@ import "strings"
 import "encoding/json"
 import "net"
 import "github.com/rainycape/geoip"
+import "github.com/olivere/elastic"
+import "log"
+import "os"
+import "context"
+import ("net/http"; "crypto/tls")
+import "hash/fnv"
+import "strconv"
+
 
 
 type Devent struct{
@@ -23,8 +31,7 @@ type Location struct{
 
 
 type GeoIP struct{
-    Continent string `json:"continent"`
-    Country string `json:"country"`
+
     Location Location `json:"location"`
 }
 
@@ -34,8 +41,45 @@ type DeventPlus struct{
     Site string `json:"site"`
     Author string `json:"author"`
     IP string `json:"ip"`
+    Continent string `json:"continent"`
+    Country string `json:"country"`
     GeoIP GeoIP `json:"geoip"`
 
+}
+
+
+
+const (
+	indexName    = "logstash"
+	docType      = "_doc"
+	appName      = "zoneh"
+	indexMapping = `{
+    "settings":{
+            "number_of_shards":1,
+            "number_of_replicas":0
+          },
+          "mappings" : {
+							"_doc" : {
+								"properties" : {
+                  "date" : { "type" : "date" },
+									"site" : { "type" : "text" , "fields": {  "raw": {  "type":  "keyword" } } },
+									"author" : { "type" : "text", "fields": {  "raw": {  "type":  "keyword" } }},
+                  "ip" : { "type" : "ip"},
+                  "continent" : { "type" : "text", "fields": {  "raw": {  "type":  "keyword" } }},
+                  "country" : { "type" : "text", "fields": {  "raw": {  "type":  "keyword" } }},
+                  "location":{"type":"geo_point"}
+								}
+							}
+						}
+					}`
+)
+
+
+
+func hash(s string) uint64 {
+        h := fnv.New64a()
+        h.Write([]byte(s))
+        return h.Sum64()
 }
 
 
@@ -54,7 +98,7 @@ func getData() <- chan []byte{
   go func() {
 
     for _, item := range channel.Item {
-      d,_:=item.PubDate.Format("2006-01-02,15:04:05")
+      d,_:=item.PubDate.Format("2006-01-02 15:04:05")
       dst:=strings.Split(item.Description," notified by ")
       s,a:=dst[0],dst[1]
       //fmt.Println(s)
@@ -70,6 +114,8 @@ func getData() <- chan []byte{
       }else{
         ip_str=ip[0].String()
       }
+      d=strings.Replace(d," ","T",-1)
+      fmt.Println(d)
       ev1 :=Devent{d,s,a,ip_str}
       jsonStr , err := json.Marshal(ev1)
       out <- jsonStr
@@ -94,9 +140,9 @@ func ETL(in <- chan []byte) <-chan []byte{
   var ContinentName,CountryName string
   //var Geopoint GeoIP
 
-  ContinentName=""
-  CountryName=""
-  PointLocation:=Location{0,0}
+  ContinentName="N/A"
+  CountryName="N/A"
+  PointLocation:=Location{181,91}
 
 
   out := make(chan []byte)
@@ -117,27 +163,36 @@ func ETL(in <- chan []byte) <-chan []byte{
 
   	    if err != nil {
           fmt.Println("LOCATION OF IP NOT FOUND")
-  	    }
+          ContinentName="N/A"
+          CountryName="N/A "
+          PointLocation=Location{181,91}
 
-        if res.Continent.Name != nil{
-          ContinentName=res.Continent.Name.String()
+  	    }else{
 
-        }else{
-          ContinentName=""
+          if res.Continent.Name != nil{
+            ContinentName=res.Continent.Name.String()
+          }else{
+            ContinentName="N/A"
+          }
+          //fmt.Println(res)
+          if res.Country.Name != nil{
+            //CountryName=string(res.Country.Name)
+            CountryName= res.Country.Name.String()
+          }else{
+            CountryName="N/A"
+          }
+          PointLocation=Location{res.Longitude,res.Latitude}
         }
-
-        //fmt.Println(res)
-        if res.Country.Name != nil{
-          //CountryName=string(res.Country.Name)
-          CountryName= res.Country.Name.String()
-        }else{
-          CountryName=""
-        }
-        PointLocation=Location{res.Longitude,res.Latitude}
       }
-      Geopoint :=GeoIP{ContinentName,CountryName,PointLocation}
-      nm := DeventPlus{m.Date,m.Site,m.Author,m.IP,Geopoint}
+
+
+      Geopoint :=GeoIP{PointLocation}
+      nm := DeventPlus{m.Date,m.Site,m.Author,m.IP,ContinentName,CountryName,Geopoint}
       jsonStr , _ := json.Marshal(nm)
+      PointLocation=Location{181,91}
+      ContinentName="N/A"
+      CountryName="N/A"
+
 
       out <- jsonStr
     }
@@ -148,20 +203,126 @@ return out
 }
 
 
-func main(){
+func output(in <- chan []byte){
 
   var tmp DeventPlus
+
+  tr := &http.Transport{
+  TLSClientConfig: &tls.Config{InsecureSkipVerify : true},
+  }
+  noCheckClient := &http.Client{Transport: tr}
+
+  //ctx:=context.Background()
+
+  client, err := elastic.NewClient(
+      elastic.SetHttpClient(noCheckClient),
+      elastic.SetURL("https://localhost:9200"),
+      elastic.SetBasicAuth("admin","admin"),
+      elastic.SetHealthcheck(false),
+      elastic.SetScheme("https"),
+      elastic.SetSniff(false),
+      elastic.SetErrorLog(log.New(os.Stderr, "", log.LstdFlags)),
+		  elastic.SetInfoLog(log.New(os.Stdout, "", log.LstdFlags)),
+      )
+
+
+  if err != nil  {
+    fmt.Println("entrei aqui")
+
+    panic(err)
+  }
+  log.Printf("client.running? %v",client.IsRunning())
+
+  fmt.Println(client)
+  //fmt.Println(ctx)
+
+// Zone-H
+  exists, err := client.IndexExists(indexName).Do(context.Background())
+
+  if err != nil {
+  	// Handle error
+    fmt.Println("ERRO EXISTs>",err)
+
+  }
+  fmt.Println(exists)
+
+  if !exists {
+  	fmt.Println("Index does not exist yet")
+    //ctx=context.Background()
+    res, err := client.CreateIndex(indexName).Body(indexMapping).Do(context.Background())
+
+    if err != nil {
+
+      fmt.Println(err)
+      //return err
+    }
+
+    if !res.Acknowledged {
+      fmt.Println("Error message from creating index: -> ",res)
+      //return errors.New("CreateIndex was not acknowledged. Check that timeout value is correct.")
+    }
+
+  }
+  //bulkRequest := client.Bulk()
+
+  for elem:= range in {
+    json.Unmarshal(elem,&tmp)
+    id:= hash(tmp.Date+tmp.Site+tmp.Author)
+    //req:= elastic.NewBulkIndexRequest().Index(indexName).Type(docType).Id(strconv.FormatUint(id,10)).Doc(elem)
+    //bulkRequest = bulkRequest.Add(req)
+
+
+
+    result,err :=client.Index().Index(indexName).Type(docType).Id(strconv.FormatUint(id,10)).BodyJson(tmp).Do(context.Background())
+    fmt.Println(result)
+    fmt.Println(err)
+
+    fmt.Println("id: ",id)
+    fmt.Println(tmp)
+    fmt.Println()
+
+  }
+
+
+
+  //bulkResponse, err := bulkRequest.Do(context.Background())
+  //if err != nil {
+	//		fmt.Println(err)
+  //}
+
+  _, err = client.Flush().Index(indexName).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+
+  //if bulkResponse != nil {
+  //  fmt.Println("BulResponse: ",bulkResponse)
+  //}
+
+
+
+}
+
+
+
+
+func main(){
+
+  //ar tmp DeventPlus
 
   out1:=getData()
   //fmt.Println(<-out1)
   out2:=ETL(out1)
 
-  for elem := range out2 {
+  output(out2)
+
+  //for elem := range out2 {
       //fmt.Println(elem)
-      json.Unmarshal(elem,&tmp)
-      fmt.Println(tmp)
-      fmt.Println()
-    }
+  //    json.Unmarshal(elem,&tmp)
+  //    fmt.Println(tmp)
+  //    fmt.Println()
+  //  }
 
 
 }
